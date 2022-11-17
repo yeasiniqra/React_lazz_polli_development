0759d8839e7ef06705305736de233f7621a45880
yeasiniqra <yeasin@iqrasys.com>
2022-11-17 19:49:53 +0600

    About change

--- src/components/AboutElement/AboutStory/AboutStoryTemplate.jsx
+++ src/components/AboutElement/AboutStory/AboutStoryTemplate.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AboutStoryTemplate = ({item}) => {
     return (
         <div className="ab-img-flex">
-            <img src={item.image} alt="About img" />
+            <img src={item.image} alt="About" />
         </div>
  
     );
